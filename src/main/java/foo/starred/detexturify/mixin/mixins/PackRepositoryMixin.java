c2dbe043b5cb4db127e783cee1fdcb4f0ed37e0d
skies-starred <github@starred.foo>
2026-08-30 16:33:03 +0100

    feat: make pack be over vanilla pack but below other packs

--- src/main/java/foo/starred/detexturify/mixin/mixins/PackRepositoryMixin.java
+++ src/main/java/foo/starred/detexturify/mixin/mixins/PackRepositoryMixin.java
@@ -1,26 +1,38 @@
 package foo.starred.detexturify.mixin.mixins;
 
-import foo.starred.detexturify.pack.HypixelPackCache;
+import com.google.common.collect.ImmutableList;
 import net.minecraft.server.packs.repository.Pack;
 import net.minecraft.server.packs.repository.PackRepository;
-import net.minecraft.server.packs.repository.RepositorySource;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.ModifyVariable;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.IntStream;
 
 @Mixin(PackRepository.class)
 public class PackRepositoryMixin {
-    @ModifyVariable(method = "<init>", at = @At("HEAD"), argsOnly = true)
-    private static RepositorySource[] detexturify$init(RepositorySource[] sources) {
-        final RepositorySource[] result = Arrays.copyOf(sources, sources.length + 1);
+    @Inject(method = "rebuildSelected", at = @At("RETURN"), cancellable = true)
+    private void detexturify$rebuildSelected(Collection<String> selectedNames, CallbackInfoReturnable<List<Pack>> cir) {
+        final List<Pack> packs = cir.getReturnValue();
+        if (packs == null) return;
+        if (packs.isEmpty()) return;
+        if (selectedNames.contains("detexturify/fallback/hypixel")) return;
 
-        result[sources.length] = onLoad -> {
-            final Pack pack = HypixelPackCache.active;
-            if (pack != null) onLoad.accept(pack);
-        };
+        final int i0 = IntStream.range(0, packs.size()).filter(i -> packs.get(i).getId().equals("detexturify/fallback/hypixel")).findFirst().orElse(-1);
+        final int i1 = IntStream.range(0, packs.size()).filter(i -> packs.get(i).getId().equals("vanilla")).findFirst().orElse(-1);
 
-        return result;
+        if (i0 == -1) return;
+        if (i1 == -1) return;
+        if (i0 == i1 + 1) return;
+
+        final List<Pack> mutable = new ArrayList<>(packs);
+        final Pack pack = mutable.remove(i0);
+        mutable.add(i0 < i1 ? i1 : i1 + 1, pack);
+
+        cir.setReturnValue(ImmutableList.copyOf(mutable));
     }
 }
